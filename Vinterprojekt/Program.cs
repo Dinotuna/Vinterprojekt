3b2c6d542daa44ad14ba19103e4b434e0477f041
Dino <dino.omicevic@elev.ga.ntig.sem>
2023-11-29 11:03:19 +0100

    skapat en figur och fått den hoppa

--- Vinterprojekt/Program.cs
+++ Vinterprojekt/Program.cs
@@ -1,22 +1,50 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
 Raylib.InitWindow(800, 600, "hello");
 Raylib.SetTargetFPS(60);
 
+Vector2 movement = new Vector2(0, 0);
 
+Rectangle PlayerRect = new Rectangle(300, 100, 64, 64);
+
+// skapa lista med rektanglar (hinder)
+
+float speed = 3;
 
 string scene = "start";
 
+int points = 0;
 
 
+while (!Raylib.WindowShouldClose())
 
-while (!Raylib.WindowShouldClose());
 {
-    if (scene == "game");
+    if (scene == "game")
     {
+        movement.X = 0;
+
+        movement.Y += 0.5f;
+
+        if (PlayerRect.Y > 300)
+        {
+            movement.Y = 0;
+        }
+
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+        {
+            movement.Y = -10;
+        }
         
+
+        PlayerRect.X += movement.X;
+        PlayerRect.Y += movement.Y;
+
+
+
+
     }
-    
+
     else if (scene == "start")
     {
         if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
@@ -25,16 +53,16 @@ while (!Raylib.WindowShouldClose());
         }
     }
 
-    // -----------Drawing-----------
+
+
+// -----------Drawing----------- \\
 
     Raylib.BeginDrawing(); 
     if (scene == "game")
     {
         Raylib.ClearBackground(Color.WHITE);
 
-
-
-
+        Raylib.DrawRectangleRec(PlayerRect, Color.BLUE);
 
     }
 
